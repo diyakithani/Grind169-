4e2eee02ef983904fff8b7e3f7227cb38884a3d4
Diya Kithani <diyakithani.2003@gmail.com>
2024-09-29 12:34:37 -0400

    twosum2 implemented :)

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-import twosum.twosum1;
+
+import twosum.*;
 
 import java.util.Arrays;
 
@@ -6,7 +7,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         twosum1 obj=new twosum1();
+        twosum2 obj1=new twosum2();
         System.out.println(Arrays.toString(obj.twosum(new int[]{1, 2, 3, 4, 5},9)));
+        System.out.println(Arrays.toString(obj1.twosum(new int[] {3, 3, 4, 5},6)));
 
     }
 }
